948dd94c01f8a4da0cf4e14794f211f3c9f2242b
H. Melis Yilmaz <hacermelisyilmaz@gmail.com>
2023-08-27 14:16:41 +0300

    1st test: info div text content is checked

--- frontend/components/App.test.js
+++ frontend/components/App.test.js
@@ -1,8 +1,20 @@
-import { render } from "@testing-library/react";
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
 import AppFunctional from "./AppFunctional";
 
 describe("AppFunctional", () => {
   beforeAll(() => {
     render(<AppFunctional />);
+  });
+
+  test("info div has the right headings", () => {
+    const coorInfo = screen.getByTestId(/coordinates-info/i);
+    expect(coorInfo).toBeInTheDocument();
+    expect(coorInfo).toHaveTextContent(/Koordinatlar /i);
+
+    const stepsInfo = screen.getByTestId(/steps-info/i);
+    expect(stepsInfo).toBeInTheDocument();
+    expect(stepsInfo).toHaveTextContent(/ kere ilerlediniz/i);
   });
 });
